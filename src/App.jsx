66862aed64bf61c29492dc09dfd33e41f7196aa0
Abdullah <abdullahblrr@gmail.com>
2024-03-12 17:36:16 +0300

    feat: filterSearch

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./index.css";
+import Table from "./Table";
+import data from "./data";
+import { useState } from "react";
+function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+
+  console.log(data[0]["first_name"]);
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
+  return (
+    <div className="app">
+      <input
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        type="text"
+        className="Search"
+        placeholder="Search..."
+      />
+      <ul className="list">
+        <Table data={search(data)} />
+      </ul>
+    </div>
+  );
+}
+
+export default App;
